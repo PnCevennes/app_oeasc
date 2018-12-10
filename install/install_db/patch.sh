650bd15ca974aac0a6e90c66da7d5b32e611240c
Joël CLEMENT <joelclems@gmail.com>
2018-12-10 13:11:13 +0100

    _

--- install/install_db/patch.sh
+++ install/install_db/patch.sh
@@ -1,7 +1,18 @@
 # patch.sh
 echo "
-UPDATE utilisateurs.bib_organismes
-SET nom_organisme='Aucun' WHERE nom_organisme LIKE 'Pas d%'
-;
+-- UPDATE utilisateurs.bib_organismes
+-- SET nom_organisme='Aucun' WHERE nom_organisme LIKE 'Pas d%'
+-- ;
+
+INSERT INTO ref_nomenclatures.t_nomenclatures(
+            id_type, cd_nomenclature, mnemonique, 
+            label_default, definition_default, 
+            label_fr, definition_fr, 
+            source)
+    VALUES (ref_nomenclatures.get_id_nomenclature_type('OEASC_DEGAT_TYPE'), 'LIEV', 'Lièv.', 
+        'Lièvre', 'Lièvre',
+        'Lièvre', 'Lièvre',
+        'OEASC')
+        ;
 
 " | $psqla
