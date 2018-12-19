0c05ffb6d32c34302f69209b5edcc6e662b68520
Joël CLEMENT <joelclems@gmail.com>
2018-12-19 16:44:14 +0100

    _

--- install/install_db/patch.sh
+++ install/install_db/patch.sh
@@ -4,3 +4,14 @@
 dir_script=install/install_db
 
 $psqla -f $dir_script/oeasc/oeasc_views.sql
+
+echo "GRANT USAGE ON SCHEMA oeasc TO $user_pg_save;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA oeasc TO $user_pg_save;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA oeasc TO $user_pg_save;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA ref_geo TO $user_pg_save;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA ref_geo TO $user_pg_save;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA utilisateurs TO $user_pg_save;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA utilisateurs TO $user_pg_save;
+    " | $psqla
+
+echo | $psqla
