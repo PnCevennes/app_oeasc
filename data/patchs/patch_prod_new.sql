dca8197ed85cc428d16e715a2f7e3b8f57f82d69
joel.clement <joelclems@gmail.com>
2020-03-26 08:33:52 +0100

    patch

--- data/patchs/patch_prod_new.sql
+++ data/patchs/patch_prod_new.sql
@@ -1,5 +1,6 @@
+DROP TABLE oeasc_commons.t_contents
 CREATE TABLE oeasc_commons.t_contents (
-	id_content INT,
+	id_content SERIAL,
     code CHARACTER VARYING,
     md text,
     meta_create_date timestamp without time zone,
@@ -8,16 +9,16 @@ CREATE TABLE oeasc_commons.t_contents (
 	CONSTRAINT pk_t_contents_id_content PRIMARY KEY (id_content)
 );
 
-CREATE TRIGGER tri_meta_dates_change_t_contents
-   BEFORE INSERT OR UPDATE
-   ON oeasc_commons.t_contents
-   FOR EACH ROW
-   EXECUTE PROCEDURE public.fct_trg_meta_dates_change();
+-- CREATE TRIGGER tri_meta_dates_change_t_contents
+--    BEFORE INSERT OR UPDATE
+--    ON oeasc_commons.t_contents
+--    FOR EACH ROW
+--    EXECUTE PROCEDURE public.fct_trg_meta_dates_change();
 
-CREATE TABLE oeasc_commons.t_communes (
-	id_commune INT,
-    code_postal CHARACTER VARYING(5),
-    nom_commune text,
-	CONSTRAINT pk_t_communes_id_commune PRIMARY KEY (id_commune)
-);
+-- CREATE TABLE oeasc_commons.t_communes (
+-- 	id_commune INT,
+--     code_postal CHARACTER VARYING(5),
+--     nom_commune text,
+-- 	CONSTRAINT pk_t_communes_id_commune PRIMARY KEY (id_commune)
+-- );
 
