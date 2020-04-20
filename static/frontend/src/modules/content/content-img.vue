260ff3da7816835b6f26110f3613964f3f4abb15
joel.clement <joelclems@gmail.com>
2020-04-20 09:04:28 +0200

    declaration_table

---
+++ static/frontend/src/modules/content/content-img.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="classes">
+    <figure>
+      <img
+        :src="src"
+        :title="`${title || src} ${source || ''}`.trim()"
+        :alt="`${title || src} ${source || ''}`.trim()"
+        :width="width"
+        :height="height"
+      />
+      <figcaption>
+          <div class="img-text">
+
+        {{ title }}
+        <i class="source" v-if="source"> - {{ source }}</i>
+          </div>
+      </figcaption>
+    </figure>
+  </div>
+</template>
+
+<script>
+import "./content.css";
+export default {
+  name: "content-img",
+  computed: {
+    classes() {
+      const classes = {
+        "img-content": true,
+        center: this.center == "",
+        float: this.float,
+        left: this.float == "left",
+        right: this.float == "right",
+        'risetitle': this['risetitle'] == "", 
+      };
+      return classes;
+    }
+  },
+  props: ["src", "height", "width", "float", "center", "title", "source", 'risetitle']
+};
+</script>
