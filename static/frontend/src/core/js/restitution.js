2773f4c64e291f0000d1072bc076aca288068d80
joel.clement <joelclems@gmail.com>
2020-04-27 16:30:10 +0200

    restitution

---
+++ static/frontend/src/core/js/restitution.js
@@ -0,0 +1,103 @@
+import * as chroma from "chroma-js";
+
+const defaults = {
+  icon: ["circle", "square", "star", "cards-diamond", "cloud"],
+  color: chroma.brewer.Dark2
+};
+
+const defaultValue = {
+  color: "lightgrey",
+  icon: "circle-outline"
+};
+
+const restitution = {
+  valueOfType(type, d, dataList, options = {}) {
+    const types = options[type] || defaults[type];
+
+    const indexElemAutres = dataList.findIndex(e => e.text == "Autres");
+    const value = this.getValue(d, options);
+    let index;
+    for (const v of value) {
+      index = dataList.findIndex(e => e.text == v);
+    }
+    let out =
+      index != -1
+        ? types[index]
+        : indexElemAutres != -1
+        ? types[indexElemAutres]
+        : null;
+    out = out || defaultValue[type];
+    return out;
+  },
+
+  color(d, dataList, options = {}) {
+    return this.valueOfType("color", d, dataList, options);
+  },
+
+  icon(d, dataList, options = {}) {
+    return this.valueOfType("icon", d, dataList, options);
+  },
+
+  getValue(d, options) {
+    let value = d;
+    if (options.replace) {
+      for (const rep of options.replace) {
+        if (value == rep[0]) {
+          value = rep[1];
+        }
+      }
+    }
+
+    value = !value
+      ? []
+      : Array.isArray(value)
+      ? value
+      : options.split
+      ? value.split(options.split)
+      : [value];
+
+    return value;
+  },
+
+  dataList(data, options) {
+    let dataList = [];
+    const name = options.name;
+    for (const d of data.filter(d1 => d1.selected)) {
+      const value = this.getValue(d[name], options);
+      for (const v of value) {
+        let elem = dataList.find(d => d.text == v);
+        if (!elem) {
+          elem = { text: v, count: 0 };
+          dataList.push(elem);
+        }
+        elem.count += 1;
+      }
+    }
+
+    dataList = dataList.sort((a, b) => b.count - a.count);
+
+    if (options.nMax) {
+      dataList = this.cutDataList(dataList, options.nMax);
+    }
+
+    return dataList;
+  },
+
+  cutDataList(dataList, nMax) {
+    const elemAutres = { text: "Autres", count: 0 };
+    const out = [];
+    for (let i = 0; i < dataList.length; i++) {
+      if (i < nMax) {
+        out.push(dataList[i]);
+      } else {
+        if (i == nMax) {
+          out.push(elemAutres);
+        }
+        elemAutres.count += 1;
+      }
+    }
+    return out;
+  }
+};
+
+export { restitution };
---
+++ static/frontend/src/modules/map/map-legend.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="legend-container" v-if="config && config.layers">
+    <div
+      class="legend"
+      v-for="[key, markerConfig] of Object.entries(config.markers || [])"
+      :key="key"
+    >
+      <div v-for="(legend, index) of markerConfig.legends || []" :key="index">
+        <template v-if="legend.title">
+          <b>{{ legend.title }}</b>
+        </template>
+        <template v-else>
+          <i
+            :style="
+              `
+            font-size: 1.8em;
+            color:${legend.color};
+          `
+            "
+            :class="`mdi mdi-${legend.icon}`"
+          ></i>
+          <span class="legendText">
+            {{ legend.text }}
+          </span>
+        </template>
+      </div>
+    </div>
+    <div class="legend">
+      <div v-if="Object.entries(config.layers || []).length">
+        <br>
+      </div>
+      <div
+        v-for="[key, layerConfig] of Object.entries(config.layers || [])"
+        :key="key"
+      >
+        <i
+          v-if="layerConfig.style"
+          :style="
+            `
+            background-color: ${getColor(
+              layerConfig.style.fillColor,
+              layerConfig.style.fillOpacity
+            )};
+            border: ${layerConfig.style.weight}px solid ${
+              layerConfig.style.color
+            };
+          `
+          "
+        ></i>
+        <span class="legendText">
+          {{ layerConfig.legend }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as chroma from "chroma-js";
+
+export default {
+  name: "mapLegend",
+  props: ["config"],
+  watch: {
+    config() {
+      // console.log('watch config legend', this.config && this.config.markers && this.config.markers.declarations && this.config.markers.declarations.legends)
+    }
+  },
+  methods: {
+    getColor(color, opacity) {
+      return chroma(color || "black").alpha(opacity);
+    }
+  }
+};
+</script>
