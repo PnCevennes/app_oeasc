4592167763c10f2eae14ff7954d911edcb30e0ae
joel.clement <joelclems@gmail.com>
2020-05-28 21:57:19 +0200

    a

--- app/modules/oeasc/i_n/api.py
+++ app/modules/oeasc/i_n/api.py
@@ -41,9 +41,11 @@ def in_valid_obs():
     )
     # setattr(obs, 'valid', data['valid'])
 
+    time.sleep(0.01)
+
     DB.session.commit()
 
-    time.sleep(0.1)
+    time.sleep(0.01)
     # DB.engine.execution_options(autocommit=True).execute(
     #     "UPDATE oeasc_in.t_observations SET valid = {} WHERE id_observation = {}"
     #     .format(data['valid'], data['id_observation'])
