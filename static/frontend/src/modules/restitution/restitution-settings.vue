7ed8d141abb5cf3c4a91ad85ccd8306ef7730f85
joel.clement <joelclems@gmail.com>
2020-09-09 17:59:19 +0200

    merge ok

--- static/frontend/src/modules/restitution/restitution-settings.vue
+++ static/frontend/src/modules/restitution/restitution-settings.vue
@@ -1,19 +1,9 @@
 <template>
   <div>
-<<<<<<< HEAD
     <div v-if="configFormRestition">
       <dynamic-form-group :config="configFormRestition" :baseModel="settings"></dynamic-form-group>
       <div class="filters" v-for="filter of filterForms" :key="filter.name">
         <dynamic-form :config="filter" :baseModel="settings.filters"></dynamic-form>
-=======
-    <div v-if="configFormConfiguration">
-      <dynamic-form-group
-        :config="configFormConfiguration"
-        :baseModel="settings"
-      ></dynamic-form-group>
-      <div class="filters" v-for="(filter, index) of filterForms" :key="index">
-        <dynamic-form :config="filter" :baseModel="filters"></dynamic-form>
->>>>>>> f6d382343a4a09fc85582a5f8320f20e91b54364
       </div>
     </div>
   </div>
@@ -40,14 +30,8 @@ export default {
   data: () => ({
     filterForms: [],
     settings: {},
-<<<<<<< HEAD
     configFormRestition: null,
     restitution: null,
-=======
-    // filters: { secteur: ["Mont Aigoual"] },
-    configFormConfiguration: null,
-    dataRestitution: null
->>>>>>> f6d382343a4a09fc85582a5f8320f20e91b54364
   }),
   mounted() {
     this.initConfig();
@@ -77,7 +61,6 @@ export default {
         return;
       }
 
-<<<<<<< HEAD
       this.restitution.setOptions(this.options());
 
       for (const formDef of Object.values(configFormRestition.formDefs)) {
@@ -88,30 +71,6 @@ export default {
         text: this.restitution.items[name].text,
         value: name,
       }));
-=======
-      if (!this.filters) {
-        this.filters = {
-          ...this.settings.filters,
-          ...(this.configRestitution.filters || {})
-        };
-        this.settings.filterList = Object.keys(this.filters);
-      }
-
-      for (const formDef of Object.values(
-        configFormConfiguration.formDefs
-      )) {
-        formDef.change = () => this.emitSettings(); // ideal newChange
-      }
-
-      let items = Object.keys(this.configRestitution.items)
-        .map(name => ({
-          text: this.configRestitution.items[name].text,
-          value: name
-        }))
-        items = items.sort((a, b) => 
-          a.text < b.text ? -1 : 1
-        );
->>>>>>> f6d382343a4a09fc85582a5f8320f20e91b54364
 
       console.log(items.map(a=>a.text))
       for (const keyForm of ["choix1", "choix2", "filterList"]) {
@@ -126,15 +85,9 @@ export default {
     },
 
     getFilterForms() {
-<<<<<<< HEAD
       return this.settings.filterList.map((name) => {
         const item = this.restitution.item(name);
         const dataList = this.restitution.dataList(name, {});
-=======
-      return this.settings.filterList.map(name => {
-        const item = restitution.getItem(name, this.configRestitution);
-        const dataList = restitution.dataList(this.dataRestitution, item);
->>>>>>> f6d382343a4a09fc85582a5f8320f20e91b54364
         return {
           type: "list_form",
           name: item.key,
@@ -162,7 +115,6 @@ export default {
         this.emitSettings();
       }, 10);
     },
-<<<<<<< HEAD
     options() {
       return {
         ...this.settings,
@@ -172,33 +124,5 @@ export default {
       this.$emit("updateSettings", this.options());
     },
   },
-=======
-    getData() {
-      this.configRestitution = this.$store.getters.configRestitution(
-        this.dataType
-      );
-      if (!this.configRestitution) {
-        return;
-      }
-
-      this.settings = {
-        data_type: this.dataType,
-        ...(this.configRestitution.default || {})
-      };
-
-      restitution.getData(this.configRestitution, this.$store).then(data => {
-        this.dataRestitution = data;
-        this.initConfig();
-      });
-    },
-    emitSettings() {
-      this.$emit("updateSettings", {
-        ...this.settings,
-        filters: copy(this.filters),
-        dataType: this.dataType
-      });
-    }
-  }
->>>>>>> f6d382343a4a09fc85582a5f8320f20e91b54364
 };
 </script>
