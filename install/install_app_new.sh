c41248ffc7637ac09ca4e0c4279c137ca6ff4e57
joel.clement <joelclems@gmail.com>
2020-12-15 15:34:54 +0100

    refactor add

--- install/install_app_new.sh
+++ install/install_app_new.sh
@@ -1,3 +1,24 @@
-# install_app_new.sh
+# env
+sudo apt update
+sudo apt install -y git
+sudo apt-get install -y --no-install-recommends apt-utils
 
 # nvm
+# make nvm available
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
+echo "Installation de Node et Npm"
+cd ../frontend
+nvm install
+nvm use
+
+echo " ############"
+echo "Installation des paquets Npm"
+npm ci --only=prod
+
+npm run build
