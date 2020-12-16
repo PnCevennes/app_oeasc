00b71d5828107406c77e2813998cca5185582107
joel.clement <joelclems@gmail.com>
2020-12-16 17:04:34 +0100

    refactor

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -2,21 +2,16 @@
 
 FLASKDIR=$(readlink -e "${0%/*}")
 
-. "$FLASKDIR"/config/settings.ini
+. config/settings.ini
 
-app_name=oeasc
+cd $FLASKDIR
 
 echo "Starting $app_name"
 
 # activate the virtualenv
-source $FLASKDIR/venv/bin/activate
+source venv/bin/activate
 
-echo FLASKDIR $FLASKDIR
 export PYTHONPATH=$FLASKDIR:$PYTHONPATH
-
-# cd $FLASKDIR
-echo $PYTHONPATH
-
 
 # # Start your unicorn
 exec gunicorn server:app --error-log $FLASKDIR/var/log/errors_develop.log --pid="${app_name}.pid" -w "${gun_num_workers}"  -b "${gun_host}:${gun_port}"  -n "${app_name}"
