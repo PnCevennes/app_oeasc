3006f102b9b5e3ab8e0cf8978f1617b4ccb34d50
joel.clement <joelclems@gmail.com>
2022-02-28 14:34:37 +0100

    +

--- frontend/src/config/config-map.js
+++ frontend/src/config/config-map.js
@@ -2,7 +2,7 @@ import { styles } from './config-style.js'
 
 
 const IGN_KEY = '3s8maqk3hm42vsjty9yoajtb';
-// const IGN_KEY2 = 'frqnaqqjemn7urmlxde3qkjj';
+const IGN_KEY2 = '4yfu5sc8fce57kufieu8hcsc';
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoiam9lbGNsZW1zIiwiYSI6ImNrbDBtaDkzcDBwZGwycG1sejQxczh0bWIifQ.LkAgAh9XQXuK1UeAcisSAA';
 const MAPBOX_ID = 'mapbox/streets-v11';
 
@@ -32,7 +32,7 @@ const configMap = {
       label: 'IGN (Cartes)'
     },
     ign_ortho: {
-      url: `https://wxs.ign.fr/${IGN_KEY}/geoportail/wmts?` +
+      url: `https://wxs.ign.fr/${IGN_KEY2}/geoportail/wmts?` +
       "&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0" +
       "&STYLE=normal" +
       "&TILEMATRIXSET=PM" +
