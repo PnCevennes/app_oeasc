7ff2eb048dc5f468105867f3b13d425b9cdc67f4
joel.clement <joelclems@gmail.com>
2022-03-04 08:16:56 +0100

    bilan from attribution

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -15,6 +15,8 @@ source venv/bin/activate
 export PYTHONPATH=$FLASKDIR:$PYTHONPATH
 
 # # Start your unicorn
-exec gunicorn server:app --error-log $FLASKDIR/var/log/errors_develop.log --pid="${app_name}.pid" -w "${gun_num_workers}"  -b "${gun_host}:${gun_port}"  -n "${app_name}"
-
+exec gunicorn server:app --error-log $FLASKDIR/var/log/errors_develop.log \
+    --pid="${app_name}.pid" \
+    -w "${gun_num_workers}"  -b "${gun_host}:${gun_port}"  -n "${app_name}" \
+    --reload
 echo done
