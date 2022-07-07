ddf18572192dcf5c9b16f1f7a3842f5d87300051
joel.clement <joelclems@gmail.com>
2022-07-07 08:10:04 +0200

    pdf

--- frontend/src/modules/export/index.js
+++ frontend/src/modules/export/index.js
@@ -1,5 +1,16 @@
 import html2pdf from "html2pdf.js";
 
+const  resetMapStyle = (elem) => {
+  const img = elem.nextElementSibling;
+  img.remove();
+  elem.classList.remove("map-img");
+  elem.style.display = "block";
+  elem.style.width = this.widthSave + "px";
+  elem.style.height = this.heightSave + "px";
+  // this._map.invalidateSize();
+  // this.reinitZoom();
+}
+
 const exportPDF = function(id, filename, $store) {
   return new Promise(resolve => {
     const opt = {
@@ -11,35 +22,36 @@ const exportPDF = function(id, filename, $store) {
     const element = document.getElementById(id);
     element.classList.add("pdf");
     setTimeout(() => {
-      
+
       const mapImgpromises = $store.getters
         .elemMapServices(id)
         .map(mapService => mapService.toImg());
 
       Promise.all(mapImgpromises).then(mapElems => {
-        
-        
-        
+        console.log('html2pdf')
         html2pdf()
           .from(element)
           .set(opt)
           .save()
           .then(() => {
             //remap the map remove imgs
-            
+            console.log('html2pdf end')
+
             mapElems;
             element.classList.remove("pdf");
+            resolve(true);
             for (const map of mapElems) {
-              
-              this.resetMapStyle(map)
+              console.log('map reset', map)
+
+              resetMapStyle(map)
               // map.style.display = "block";
-              
+
               // // map.parentElement.removeChild(map.nextSibling);
               // map.classList.remove("map-img");
             }
 
-            
-            resolve();
+            console.log('resolve')
+            resolve(true);
           });
       });
     }, 1000);
