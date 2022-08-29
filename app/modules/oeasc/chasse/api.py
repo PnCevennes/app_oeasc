a8a00420c111218a502b5ab27e08d5f5eadb66d2
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-08-29 17:19:07 +0200

    Format Black

--- app/modules/oeasc/chasse/api.py
+++ app/modules/oeasc/chasse/api.py
@@ -1,11 +1,19 @@
-'''
+"""
 api chasse
-'''
+"""
 
 from .models import (
-    TPersonnes, TZoneCynegetiques, TZoneIndicatives,
-    TLieuTirs, TLieuTirSynonymes, TSaisons, TSaisonDates,
-    TAttributionMassifs, TTypeBracelets, TAttributions, TRealisationsChasse,
+    TPersonnes,
+    TZoneCynegetiques,
+    TZoneIndicatives,
+    TLieuTirs,
+    TLieuTirSynonymes,
+    TSaisons,
+    TSaisonDates,
+    TAttributionMassifs,
+    TTypeBracelets,
+    TAttributions,
+    TRealisationsChasse,
 )
 from ..generic.definitions import GenericRouteDefinitions
 from ..generic.repository import getlist
@@ -18,7 +26,7 @@ from .repositories import (
     chasse_process_args,
     chasse_get_infos,
     get_data_export_ods,
-    get_data_all_especes_export_ods
+    get_data_all_especes_export_ods,
 )
 from sqlalchemy import column, select, func, table, distinct, over
 import json
@@ -27,98 +35,63 @@ from app.utils.env import ROOT_DIR
 from py3o.template import Template
 
 config = current_app.config
-DB = config['DB']
+DB = config["DB"]
 
 
-bp = Blueprint('chasse_api', __name__)
+bp = Blueprint("chasse_api", __name__)
 grd = GenericRouteDefinitions()
 
-droits = { 'C': 4, 'R': 0, 'U': 4, 'D': 4 }
+droits = {"C": 4, "R": 0, "U": 4, "D": 4}
 
 definitions = {
-    'personne': {
-        'model': TPersonnes,
-        'droits': droits
-    },
-    'zone_cynegetique': {
-        'model': TZoneCynegetiques,
-        'droits': droits
-    },
-    'zone_cynegetique': {
-        'model': TZoneCynegetiques,
-        'droits': droits
-    },
-    'zone_indicative': {
-        'model': TZoneIndicatives,
-        'droits': droits
-    },
-    'lieu_tir': {
-        'model': TLieuTirs,
-        'droits': droits
-    },
-    'lieu_tir_synonyme': {
-        'model': TLieuTirSynonymes,
-        'droits': droits
-    },
-    'saison': {
-        'model': TSaisons,
-        'droits': droits
-    },
-    'saison_date': {
-        'model': TSaisonDates,
-        'droits': droits
-    },
-    'attribution_massif': {
-        'model': TAttributionMassifs,
-        'droits': droits
-    },
-    'type_bracelet': {
-        'model': TTypeBracelets,
-        'droits': droits
-    },
-    'attribution': {
-        'model': TAttributions,
-        'droits': droits
-    },
-    'realisation': {
-        'model': TRealisationsChasse,
-        'droits': droits
-    },
+    "personne": {"model": TPersonnes, "droits": droits},
+    "zone_cynegetique": {"model": TZoneCynegetiques, "droits": droits},
+    "zone_cynegetique": {"model": TZoneCynegetiques, "droits": droits},
+    "zone_indicative": {"model": TZoneIndicatives, "droits": droits},
+    "lieu_tir": {"model": TLieuTirs, "droits": droits},
+    "lieu_tir_synonyme": {"model": TLieuTirSynonymes, "droits": droits},
+    "saison": {"model": TSaisons, "droits": droits},
+    "saison_date": {"model": TSaisonDates, "droits": droits},
+    "attribution_massif": {"model": TAttributionMassifs, "droits": droits},
+    "type_bracelet": {"model": TTypeBracelets, "droits": droits},
+    "attribution": {"model": TAttributions, "droits": droits},
+    "realisation": {"model": TRealisationsChasse, "droits": droits},
 }
 
-grd.add_generic_routes('chasse', definitions)
+grd.add_generic_routes("chasse", definitions)
 
 
-
-@bp.route('results/bilan', methods=['GET'])
+@bp.route("results/bilan", methods=["GET"])
 @json_resp
 def chasse_bilan():
-    '''
-        route pour le bilan chasse
-    '''
+    """
+    route pour le bilan chasse
+    """
 
     params = chasse_process_args()
 
     return get_chasse_bilan(params)
 
-@bp.route('results/ice', methods=['GET'])
+
+@bp.route("results/ice", methods=["GET"])
 @json_resp
 def api_result_ice():
-    '''
-        API ICE
-    '''
+    """
+    API ICE
+    """
 
     params = chasse_process_args()
 
     req = func.oeasc_chasse.fct_calcul_ice_mc(
-        params['id_espece'],
-        params['id_zone_indicative'],
-        params['id_zone_cynegetique'],
-        params['id_secteur'],
-        params['poids_ou_dagues']
+        params["id_espece"],
+        params["id_zone_indicative"],
+        params["id_zone_cynegetique"],
+        params["id_secteur"],
+        params["poids_ou_dagues"],
     )
     res = DB.engine.execute(req).first()[0]
     return res
+
 
 # @bp.route('results/realisation', methods=['GET'])
 # @json_resp
@@ -127,71 +100,69 @@ def api_result_ice():
 #     columns = GenericTable('v_pre_bilan_pretty', 'oeasc_chasse', DB.engine).tableDef.columns
 
 
-@bp.route('results/infos', methods=['GET'])
+@bp.route("results/infos", methods=["GET"])
 @json_resp
 def api_chasse_result_info():
-    '''
-    '''
+    """ """
     return chasse_get_infos()
 
-@bp.route('results/attribution_bracelet', methods=['GET'])
+
+@bp.route("results/attribution_bracelet", methods=["GET"])
 @json_resp
 def api_result_custom():
-    '''
-        API CUSTOM
-    '''
+    """
+    API CUSTOM
+    """
 
     params = chasse_process_args()
 
     out = {}
 
-    for bracelet in ['CEM', 'CEFF', 'CEFFD']:
-        params['bracelet'] = bracelet
-        data= get_attribution_result(params)
+    for bracelet in ["CEM", "CEFF", "CEFFD"]:
+        params["bracelet"] = bracelet
+        data = get_attribution_result(params)
         out[bracelet] = data
     return out
 
 
-
-@bp.route('export/csv/', methods=['GET'])
+@bp.route("export/csv/", methods=["GET"])
 @csv_resp
 def api_result_export():
-    '''
-        API CUSTOM
-    '''
+    """
+    API CUSTOM
+    """
 
     # gestion paramètres
-    data_type = request.args.get('data_type')
-    filters = getlist(request.args, 'filters')
+    data_type = request.args.get("data_type")
+    filters = getlist(request.args, "filters")
 
-    views = {
-        'realisation': 'oeasc_chasse.v_export_realisation_csv'
-    }
+    views = {"realisation": "oeasc_chasse.v_export_realisation_csv"}
 
     view = views.get(data_type)
-    schema_name = view.split('.')[0]
-    table_name = view.split('.')[1]
+    schema_name = view.split(".")[0]
+    table_name = view.split(".")[1]
 
     # view + filters
-    results = (
-        GenericQuery(DB, schemaName=schema_name, tableName=table_name, filters=filters, limit=1e6)
-        .return_query()
+    results = GenericQuery(
+        DB, schemaName=schema_name, tableName=table_name, filters=filters, limit=1e6
+    ).return_query()
+    data = results["items"]
+    file_name = "export_{}_{}".format(
+        data_type, datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%s")
     )
-    data = results['items']
-    file_name = 'export_{}_{}'.format(data_type, datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%s"))
     return (file_name, data, data[0].keys(), ";")
 
 
-@bp.route('export/ods', methods=['GET'])
+@bp.route("export/ods", methods=["GET"])
 def api_chasse_ods():
-    '''
-        test export ods
-    '''
+    """
+    test export ods
+    """
 
-    template_path = ROOT_DIR / 'app/templates/ods/template_bilan_chasse.ods'
-    output_path = ROOT_DIR / 'static/export/test.ods'
+    template_path = ROOT_DIR / "app/templates/ods/template_bilan_chasse.ods"
+    output_path = ROOT_DIR / "static/export/test.ods"
 
-    data = get_data_all_especes_export_ods('2021-2022')
+    data = get_data_all_especes_export_ods("2021-2022")
     # return jsonify(data)
     # data = {
     #     "nom_saison": "2021-2022",
--- app/modules/oeasc/chasse/models.py
+++ app/modules/oeasc/chasse/models.py
@@ -1,6 +1,6 @@
-'''
+"""
     modele chasse
-'''
+"""
 
 from flask import current_app
 from utils_flask_sqla.serializers import serializable
@@ -12,16 +12,17 @@ from sqlalchemy.sql.expression import case
 from ..commons.models import TEspeces, TNomenclatures, TSecteurs
 
 config = current_app.config
-DB = config['DB']
+DB = config["DB"]
 
 
 @serializable
 class TPersonnes(DB.Model):
-    '''
-        Personne (tir & constat)
-    '''
-    __tablename__ = 't_personnes'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    """
+    Personne (tir & constat)
+    """
+
+    __tablename__ = "t_personnes"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_personne = DB.Column(DB.Integer, primary_key=True)
     nom_personne = DB.Column(DB.Unicode)
@@ -29,72 +30,78 @@ class TPersonnes(DB.Model):
 
 @serializable
 class TZoneCynegetiques(DB.Model):
-    '''
-        Zones Cynegetiques
-    '''
-    __tablename__ = 't_zone_cynegetiques'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    """
+    Zones Cynegetiques
+    """
+
+    __tablename__ = "t_zone_cynegetiques"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_zone_cynegetique = DB.Column(DB.Integer, primary_key=True)
     code_zone_cynegetique = DB.Column(DB.Unicode)
     nom_zone_cynegetique = DB.Column(DB.Unicode)
     id_secteur = DB.Column(
-        DB.Integer,
-        DB.ForeignKey('oeasc_commons.t_secteurs.id_secteur')
+        DB.Integer, DB.ForeignKey("oeasc_commons.t_secteurs.id_secteur")
     )
     secteur = DB.relationship(TSecteurs, foreign_keys=id_secteur)
 
 
 @serializable
 class TZoneIndicatives(DB.Model):
-    '''
-        Zones d'interet cynegetiques
-    '''
-    __tablename__ = 't_zone_indicatives'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    """
+    Zones d'interet cynegetiques
+    """
+
+    __tablename__ = "t_zone_indicatives"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_zone_indicative = DB.Column(DB.Integer, primary_key=True)
     code_zone_indicative = DB.Column(DB.Unicode)
     nom_zone_indicative = DB.Column(DB.Unicode)
     id_zone_cynegetique = DB.Column(
         DB.Integer,
-        DB.ForeignKey('oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique')
+        DB.ForeignKey("oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique"),
     )
-    zone_cynegetique = DB.relationship(TZoneCynegetiques, foreign_keys=id_zone_cynegetique)
+    zone_cynegetique = DB.relationship(
+        TZoneCynegetiques, foreign_keys=id_zone_cynegetique
+    )
     geom = DB.Column(Geometry)
+
 
 @serializable
 class TLieuTirs(DB.Model):
-    '''
-        Lieu de tir
-    '''
-    __tablename__ = 't_lieu_tirs'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    """
+    Lieu de tir
+    """
+
+    __tablename__ = "t_lieu_tirs"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_lieu_tir = DB.Column(DB.Integer, primary_key=True)
     code_lieu_tir = DB.Column(DB.Unicode)
     nom_lieu_tir = DB.Column(DB.Unicode)
     geom = DB.Column(Geometry)
-    id_area_commune = DB.Column(DB.Integer, DB.ForeignKey('ref_geo.l_areas.id_area'))
+    id_area_commune = DB.Column(DB.Integer, DB.ForeignKey("ref_geo.l_areas.id_area"))
     label_commune = DB.Column(DB.Unicode)
     id_zone_indicative = DB.Column(
-        DB.Integer,
-        DB.ForeignKey('oeasc_chasse.t_zone_indicatives.id_zone_indicative')
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_zone_indicatives.id_zone_indicative")
     )
     zone_indicative = DB.relationship(TZoneIndicatives, foreign_keys=id_zone_indicative)
 
 
 @serializable
 class TLieuTirSynonymes(DB.Model):
-    '''
-        Lieu de tir synonymes
-    '''
+    """
+    Lieu de tir synonymes
+    """
 
-    __tablename__ = 't_lieu_tir_synonymes'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    __tablename__ = "t_lieu_tir_synonymes"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_lieu_tir_synonyme = DB.Column(DB.Integer, primary_key=True)
-    id_lieu_tir = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_lieu_tirs.id_lieu_tir'))
+    id_lieu_tir = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_lieu_tirs.id_lieu_tir")
+    )
     nom_lieu_tir_synonyme = DB.Column(DB.Unicode)
 
     lieu_tir = DB.relationship(TLieuTirs)
@@ -105,15 +112,14 @@ class TLieuTirSynonymes(DB.Model):
     )
 
 
-
 @serializable
 class TSaisons(DB.Model):
-    '''
-        Saison de chasse
-    '''
+    """
+    Saison de chasse
+    """
 
-    __tablename__ = 't_saisons'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    __tablename__ = "t_saisons"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_saison = DB.Column(DB.Integer, primary_key=True)
     nom_saison = DB.Column(DB.Unicode)
@@ -122,40 +128,51 @@ class TSaisons(DB.Model):
     current = DB.Column(DB.Boolean)
     commentaire = DB.Column(DB.Unicode)
 
+
 @serializable
 class TSaisonDates(DB.Model):
-    '''
-        Date de chasse des différentes especes
-    '''
+    """
+    Date de chasse des différentes especes
+    """
 
-    __tablename__ = 't_saison_dates'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    __tablename__ = "t_saison_dates"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_saison_date = DB.Column(DB.Integer, primary_key=True)
-    id_saison = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_saisons.id_saison'))
+    id_saison = DB.Column(DB.Integer, DB.ForeignKey("oeasc_chasse.t_saisons.id_saison"))
     saison = DB.relationship(TSaisons, foreign_keys=id_saison)
-    id_espece = DB.Column(DB.Integer, DB.ForeignKey('oeasc_commons.t_especes.id_espece'))
+    id_espece = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_commons.t_especes.id_espece")
+    )
     espece = DB.relationship(TEspeces, foreign_keys=id_espece)
     date_debut = DB.Column(DB.Date)
     date_fin = DB.Column(DB.Date)
-    id_nomenclature_type_chasse = DB.Column(DB.Integer, DB.ForeignKey('ref_nomenclatures.t_nomenclatures.id_nomenclature'))
-    nomenclature_type_chasse = DB.relationship(TNomenclatures, foreign_keys=id_nomenclature_type_chasse)
-
+    id_nomenclature_type_chasse = DB.Column(
+        DB.Integer, DB.ForeignKey("ref_nomenclatures.t_nomenclatures.id_nomenclature")
+    )
+    nomenclature_type_chasse = DB.relationship(
+        TNomenclatures, foreign_keys=id_nomenclature_type_chasse
+    )
 
 
 @serializable
 class TAttributionMassifs(DB.Model):
-    '''
-        Attribution selon les années
-    '''
+    """
+    Attribution selon les années
+    """
 
-    __tablename__ = 't_attribution_massifs'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    __tablename__ = "t_attribution_massifs"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_attribution_massif = DB.Column(DB.Integer, primary_key=True)
-    id_saison = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_saisons.id_saison'))
-    id_espece = DB.Column(DB.Integer, DB.ForeignKey('oeasc_commons.t_especes.id_espece'))
-    id_zone_cynegetique = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique'))
+    id_saison = DB.Column(DB.Integer, DB.ForeignKey("oeasc_chasse.t_saisons.id_saison"))
+    id_espece = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_commons.t_especes.id_espece")
+    )
+    id_zone_cynegetique = DB.Column(
+        DB.Integer,
+        DB.ForeignKey("oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique"),
+    )
     nb_affecte_min = DB.Column(DB.Integer)
     nb_affecte_max = DB.Column(DB.Integer)
 
@@ -163,21 +180,31 @@ class TAttributionMassifs(DB.Model):
     espece = DB.relationship(TEspeces)
     zone_cynegetique = DB.relationship(TZoneCynegetiques)
 
+
 @serializable
 class VPlanChasseRealisationBilan(DB.Model):
-    '''
-        Attribution selon les années
-    '''
+    """
+    Attribution selon les années
+    """
 
-    __tablename__ = 'v_plan_chasse_realisation_bilan'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    __tablename__ = "v_plan_chasse_realisation_bilan"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_attribution_massif = DB.Column(DB.Integer, primary_key=True)
-    id_saison = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_saisons.id_saison'))
-    id_espece = DB.Column(DB.Integer, DB.ForeignKey('oeasc_commons.t_especes.id_espece'))
-    id_secteur = DB.Column(DB.Integer, DB.ForeignKey('oeasc_commons.t_secteurs.id_secteur'))
-    id_zone_cynegetique = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique'))
-    id_zone_indicative = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_zone_indicatives.id_zone_indicative'))
+    id_saison = DB.Column(DB.Integer, DB.ForeignKey("oeasc_chasse.t_saisons.id_saison"))
+    id_espece = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_commons.t_especes.id_espece")
+    )
+    id_secteur = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_commons.t_secteurs.id_secteur")
+    )
+    id_zone_cynegetique = DB.Column(
+        DB.Integer,
+        DB.ForeignKey("oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique"),
+    )
+    id_zone_indicative = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_zone_indicatives.id_zone_indicative")
+    )
     nb_affecte_min = DB.Column(DB.Integer)
     nb_affecte_max = DB.Column(DB.Integer)
     nb_realisation = DB.Column(DB.Integer)
@@ -189,17 +216,20 @@ class VPlanChasseRealisationBilan(DB.Model):
     zone_indicative = DB.relationship(TZoneIndicatives)
     secteur = DB.relationship(TSecteurs)
 
+
 @serializable
 class TTypeBracelets(DB.Model):
-    '''
-        Type de bracelet
-    '''
+    """
+    Type de bracelet
+    """
 
-    __tablename__ = 't_type_bracelets'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    __tablename__ = "t_type_bracelets"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_type_bracelet = DB.Column(DB.Integer, primary_key=True)
-    id_espece = DB.Column(DB.Integer, DB.ForeignKey('oeasc_commons.t_especes.id_espece'))
+    id_espece = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_commons.t_especes.id_espece")
+    )
     espece = DB.relationship(TEspeces, foreign_keys=id_espece)
     code_type_bracelet = DB.Column(DB.Unicode)
     description_type_bracelet = DB.Column(DB.Unicode)
@@ -207,19 +237,26 @@ class TTypeBracelets(DB.Model):
 
 @serializable
 class TAttributions(DB.Model):
-    '''
-        Attributions
-    '''
+    """
+    Attributions
+    """
 
-    __tablename__ = 't_attributions'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    __tablename__ = "t_attributions"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
     id_attribution = DB.Column(DB.Integer, primary_key=True)
-    id_type_bracelet = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_type_bracelets.id_type_bracelet'))
-    id_saison = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_saisons.id_saison'))
+    id_type_bracelet = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_type_bracelets.id_type_bracelet")
+    )
+    id_saison = DB.Column(DB.Integer, DB.ForeignKey("oeasc_chasse.t_saisons.id_saison"))
     numero_bracelet = DB.Column(DB.Unicode)
-    id_zone_cynegetique_affectee = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique'))
-    id_zone_indicative_affectee = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_zone_indicatives.id_zone_indicative'))
+    id_zone_cynegetique_affectee = DB.Column(
+        DB.Integer,
+        DB.ForeignKey("oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique"),
+    )
+    id_zone_indicative_affectee = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_zone_indicatives.id_zone_indicative")
+    )
     meta_create_date = DB.Column(DB.DateTime)
     meta_update_date = DB.Column(DB.DateTime)
 
@@ -231,56 +268,69 @@ class TAttributions(DB.Model):
 
 @serializable
 class TRealisationsChasse(DB.Model):
-    '''
-        Realisations
-    '''
+    """
+    Realisations
+    """
 
-    __tablename__ = 't_realisations'
-    __table_args__ = {'schema': 'oeasc_chasse', 'extend_existing': True}
+    __tablename__ = "t_realisations"
+    __table_args__ = {"schema": "oeasc_chasse", "extend_existing": True}
 
-    id_realisation = DB.Column(DB.Integer, primary_key = True)
-    id_attribution = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_attributions.id_attribution'))
+    id_realisation = DB.Column(DB.Integer, primary_key=True)
+    id_attribution = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_attributions.id_attribution")
+    )
     attribution = DB.relationship(TAttributions)
 
     saison = DB.relationship(
         TSaisons,
-        secondary='oeasc_chasse.t_attributions',
+        secondary="oeasc_chasse.t_attributions",
         primaryjoin="TAttributions.id_attribution==TRealisationsChasse.id_attribution",
         secondaryjoin="TAttributions.id_saison==TSaisons.id_saison",
         uselist=False,
-        viewonly=True
+        viewonly=True,
     )
 
-    id_auteur_tir = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_personnes.id_personne'))
+    id_auteur_tir = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_personnes.id_personne")
+    )
     auteur_tir = DB.relationship(TPersonnes, foreign_keys=id_auteur_tir)
 
-    id_auteur_constat = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_personnes.id_personne'))
+    id_auteur_constat = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_personnes.id_personne")
+    )
     auteur_constat = DB.relationship(TPersonnes, foreign_keys=id_auteur_constat)
 
-
-    id_zone_cynegetique_realisee = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique'))
+    id_zone_cynegetique_realisee = DB.Column(
+        DB.Integer,
+        DB.ForeignKey("oeasc_chasse.t_zone_cynegetiques.id_zone_cynegetique"),
+    )
     zone_cynegetique_realisee = DB.relationship(TZoneCynegetiques)
     zone_cynegetique_affectee = DB.relationship(
         TZoneCynegetiques,
-        secondary='oeasc_chasse.t_attributions',
+        secondary="oeasc_chasse.t_attributions",
         primaryjoin="TAttributions.id_attribution==TRealisationsChasse.id_attribution",
         secondaryjoin="TAttributions.id_zone_cynegetique_affectee==TZoneCynegetiques.id_zone_cynegetique",
         uselist=False,
-        viewonly=True
+        viewonly=True,
     )
 
-    id_zone_indicative_realisee = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_zone_indicatives.id_zone_indicative'))
+    id_zone_indicative_realisee = DB.Column(
+        DB.Integer, DB.ForeignKey("oeasc_chasse.t_zone_indicatives.id_zone_indicative")
+    )
     zone_indicative_realisee = DB.relationship(TZoneIndicatives)
     zone_indicative_affectee = DB.relationship(
         TZoneIndicatives,
-        secondary='oeasc_chasse.t_attributions',
+        secondary="oeasc_chasse.t_attributions",
         primaryjoin="TAttributions.id_attribution==TRealisationsChasse.id_attribution",
         secondaryjoin="TAttributions.id_zone_indicative_affectee==TZoneIndicatives.id_zone_indicative",
         uselist=False,
-        viewonly=True
+        viewonly=True,
     )
 
-    id_lieu_tir_synonyme = DB.Column(DB.Integer, DB.ForeignKey('oeasc_chasse.t_lieu_tir_synonymes.id_lieu_tir_synonyme'))
+    id_lieu_tir_synonyme = DB.Column(
+        DB.Integer,
+        DB.ForeignKey("oeasc_chasse.t_lieu_tir_synonymes.id_lieu_tir_synonyme"),
+    )
     lieu_tir_synonyme = DB.relationship(TLieuTirSynonymes)
 
     date_exacte = DB.Column(DB.Date)
@@ -289,11 +339,19 @@ class TRealisationsChasse(DB.Model):
     mortalite_hors_pc = DB.Column(DB.Boolean)
     parcelle_onf = DB.Column(DB.Boolean)
 
-    id_nomenclature_sexe = DB.Column(DB.Integer, DB.ForeignKey('ref_nomenclatures.t_nomenclatures.id_nomenclature'))
-    nomenclature_sexe = DB.relationship(TNomenclatures, foreign_keys=id_nomenclature_sexe)
+    id_nomenclature_sexe = DB.Column(
+        DB.Integer, DB.ForeignKey("ref_nomenclatures.t_nomenclatures.id_nomenclature")
+    )
+    nomenclature_sexe = DB.relationship(
+        TNomenclatures, foreign_keys=id_nomenclature_sexe
+    )
 
-    id_nomenclature_classe_age = DB.Column(DB.Integer, DB.ForeignKey('ref_nomenclatures.t_nomenclatures.id_nomenclature'))
-    nomenclature_classe_age = DB.relationship(TNomenclatures, foreign_keys=id_nomenclature_classe_age)
+    id_nomenclature_classe_age = DB.Column(
+        DB.Integer, DB.ForeignKey("ref_nomenclatures.t_nomenclatures.id_nomenclature")
+    )
+    nomenclature_classe_age = DB.relationship(
+        TNomenclatures, foreign_keys=id_nomenclature_classe_age
+    )
 
     poid_entier = DB.Column(DB.Float)
     poid_vide = DB.Column(DB.Float)
@@ -309,8 +367,12 @@ class TRealisationsChasse(DB.Model):
 
     gestation = DB.Column(DB.Boolean)
 
-    id_nomenclature_mode_chasse = DB.Column(DB.Integer, DB.ForeignKey('ref_nomenclatures.t_nomenclatures.id_nomenclature'))
-    nomenclature_mode_chasse = DB.relationship(TNomenclatures, foreign_keys=id_nomenclature_mode_chasse)
+    id_nomenclature_mode_chasse = DB.Column(
+        DB.Integer, DB.ForeignKey("ref_nomenclatures.t_nomenclatures.id_nomenclature")
+    )
+    nomenclature_mode_chasse = DB.relationship(
+        TNomenclatures, foreign_keys=id_nomenclature_mode_chasse
+    )
 
     commentaire = DB.Column(DB.Unicode)
 
@@ -323,29 +385,33 @@ class TRealisationsChasse(DB.Model):
     # meta_create_date = DB.Column(DB.DateTime)
     # meta_update_date = DB.Column(DB.DateTime)
 
-'''
+
+"""
 relation : TAttributions.realisation
 
 ou
 
 column_property : TAttributions.has_realisation
-'''
+"""
 
 # TAttributions.realisation = DB.relationship(TRealisationsChasse)
-TAttributions.has_realisation = column_property(exists().where(TRealisationsChasse.id_attribution == TAttributions.id_attribution))
+TAttributions.has_realisation = column_property(
+    exists().where(TRealisationsChasse.id_attribution == TAttributions.id_attribution)
+)
 
 TAttributions = serializable(TAttributions)
 
+
 @serializable
 class VChasseBilan(DB.Model):
-    '''
-        Realisations
-    '''
+    """
+    Realisations
+    """
 
-    __tablename__ = 'v_bilan_pretty'
+    __tablename__ = "v_bilan_pretty"
     __table_args__ = {
-        'schema': 'oeasc_chasse',
-        'extend_existing': True,
+        "schema": "oeasc_chasse",
+        "extend_existing": True,
     }
 
     id_espece = DB.Column(DB.Integer(), primary_key=True)
