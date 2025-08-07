faf3ac9f165dc102506495bf1fa9d865bdaf917d
fargo48 <meanthibaut@gmail.com>
2025-08-07 15:41:20 +0200

    correction date bandeau actualité

--- frontend/src/modules/content/utils.js
+++ frontend/src/modules/content/utils.js
@@ -1,5 +1,11 @@
 export default {
     displayDateFr(date) {
-        return date.split(' ')[0].split('-').reverse().join('/');
+        if (!date) return '';
+        const d = new Date(date);
+        if (isNaN(d)) return '';
+        const day = String(d.getDate()).padStart(2, '0');
+        const month = String(d.getMonth() + 1).padStart(2, '0');
+        const year = d.getFullYear();
+        return `${day}/${month}/${year}`;
     }
 }
